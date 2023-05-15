deb2821afb5c68448120f480d79a4941d8c9ac29
Samin <sazinsamin50@gmail.com>
2023-05-15 09:30:35 +0600

    styling

--- src/index.js
+++ src/index.js
@@ -3,17 +3,28 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 const name = 'Orca Solutions';
-const intro = 'Software solutions';
+const intro = 'A software solution is typically custom-built or configured to solve a specific customer problem. ';
 const date = new Date();
 const currentDate = date.getDate();
 const currentMonth = date.getMonth();
 const currentYear = date.getFullYear();
 
+const style = {
+  color: "red", 
+  fontSize: "2rem",
+  textAlign: "center",
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <div>
-    <h1>{name}</h1>
-    <h2>{intro}</h2>
-    <h2>{currentDate + "." + currentMonth + "." + currentYear}</h2>
+    {/*inline styling*/}
+    <h1 style={{color: "blue", textAlign: "center"}}>{name}</h1>
+    {/*inline styling from object*/}
+    <h2 style={style}>{intro}</h2>
+    {/*from public folder styling sheet*/}
+    <h2 className='dateStyle'>{currentDate + "." + currentMonth + "." + currentYear}</h2>
+    {/*style from src folder*/}
+    <h3 className='websiteStyle'>orcasolution.com</h3>
   </div>
 );
