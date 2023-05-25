2ff2b81d19368087c42d25b525e75d5046595b8d
Samin <sazinsamin50@gmail.com>
2023-05-25 21:31:57 +0600

    useEffect fetch

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # ReactJs  
-## useEffect hooks  
-[Video Anisul islam](https://www.youtube.com/watch?v=XEU3jlV9syI&list=PLgH5QX0i9K3rGtitufynBKMy5gAFpa1y8&index=42)  
-what is side effect -> Data fetching, setting up a subscription, and manually changing the DOM in React components are all examples of side effects. - React documentation.  
-https://legacy.reactjs.org/docs/hooks-effect.html  
+## useEffect fetch data  
+[Anisul islam video](https://www.youtube.com/watch?v=Z-EkslDJTJI&list=PLgH5QX0i9K3rGtitufynBKMy5gAFpa1y8&index=44)
--- src/useEffect/useEffect.js
+++ src/useEffect/useEffect.js
@@ -1,41 +1,28 @@
 import React, { useEffect, useState } from "react";
 
+
 export default function UseEffectExample() {
-  const [count1, setCount1] = useState(0);
-  const [count2, setCount2] = useState(0);
 
-  const increment1 = () => {
-    setCount1(count1 + 1);
-  };
+  const [todos, setTodos] = useState(null);
 
-  const increment2 = () => {
-    setCount2(count2 + 1);
-  };
+  const fetchData = async () => {
+    const response = await fetch("https://jsonplaceholder.typicode.com/todos");
+    const json = await response.json();
+    setTodos(json);
+  }
+  // If we want to console.log the data ->
+  // https://github.com/SazinSamin/ReactJs/tree/main/Problem&Solution/asynchronous_fetch
 
-  // this useEffect only work when we increse the count1 value and
-  // the UI will re-render.
-  // incresing counter2 won't effect becasue it doesn't bind with the
-  // useEffect
   useEffect(() => {
-    console.log("UseEffect...");
-  }, [count1]);
-  // To work useEffect every time we can omit the second parameter in useEffect
-  // remove the emepty array
-  // The second parameter of useEffect the arary, if we keep it empty it will
-  // just render first time and do noting after that.
+    fetchData();
+  }, []);
 
-  return (
-    <div>
-      <h1>UseEffectExample</h1>
-      <p>{console.log("Rendering...")}</p>
-      <div>
-        <p>{count1}</p>
-        <button onClick={increment1}>+</button>
-      </div>
-      <div>
-        <p>{count2}</p>
-        <button onClick={increment2}>+</button>
-      </div>
-    </div>
-  );
+  
+
+  return (<div>
+    <h1>Todos</h1>
+    {todos && todos.map((todo) => {
+      return (<p key={todo.id}>{todo.title}</p>)
+    })}
+  </div>)
 }
