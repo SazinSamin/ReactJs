38cdc59ad84bcb96890cf93874873500da1593de
Sazin Rishad Samin <sazinsamin50@gmail.com>
2023-06-13 12:02:11 +0600

    Create trigger_AOS_animation_whe.mdn_scroll_by_we_want.js

---
+++ Design/trigger_AOS_animation_whe.mdn_scroll_by_we_want.js
@@ -0,0 +1,60 @@
+// To trigger the animation when only a specific section is visible on the website, you can use the Intersection Observer API. 
+// This API allows you to detect when an element enters or exits the viewport.Here's an example of how you can modify your code
+// to trigger the AOS animation when the section becomes visible:
+
+```
+import React, { useEffect, useRef } from "react";
+import Aos from "aos";
+import "aos/dist/aos.css";
+
+const HomePage = () => {
+  const sectionRef = useRef(null);
+
+  useEffect(() => {
+    const options = {
+      threshold: 0.2, // Adjust this threshold value as needed
+    };
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          Aos.init({
+            duration: 1000,
+            easing: "ease-in-sine",
+            delay: 10,
+          });
+        }
+      });
+    }, options);
+
+    if (sectionRef.current) {
+      observer.observe(sectionRef.current);
+    }
+
+    return () => {
+      if (sectionRef.current) {
+        observer.unobserve(sectionRef.current);
+      }
+    };
+  }, []);
+
+  return (
+    <div>
+      {/* Other components */}
+      <div ref={sectionRef} data-aos="zoom-in">
+        {/* Content of the section */}
+      </div>
+      {/* Other components */}
+    </div>
+  );
+};
+
+export default HomePage;
+```
+// In this code, a sectionRef is created using the useRef hook to reference the section element that you want to observe. 
+// The Intersection Observer API is used in the useEffect hook to detect when the section becomes visible.
+// When the section intersects the viewport by the specified threshold (in this example, 20% of the section is visible), 
+// the AOS animation is triggered.Make sure to adjust the threshold value to your desired visibility criteria. Additionally,
+// ensure that the section you want to animate has the data-aos attribute with the desired AOS animation name.By using 
+// the Intersection Observer API, you can trigger the AOS animation when the section is visible, providing a more precise 
+// control over when the animation should occur.
